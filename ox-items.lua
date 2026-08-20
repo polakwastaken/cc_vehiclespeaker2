b28e968f7ec1fa07d0fd5f779a39d17abacd7812
Polak <126923982+polakwastaken@users.noreply.github.com>
2026-08-20 10:25:07 +0200

    eintrag in die items.lua von ox für das item

---
+++ ox-items.lua
@@ -0,0 +1,13 @@
+	},
+
+	['vehiclespeaker'] = {
+		label = 'Fahrzeug-Lautsprecher',
+		weight = 500,
+		stack = false,
+		close = true,
+		allowArmed = true,
+		description = 'Als Fahrer benutzen, um ihn in das aktuelle Fahrzeug einzubauen. Erneut benutzen, um ihn wieder auszubauen.',
+		client = {
+			event = 'cc_vehiclespeaker:useItem',
+		},
+	},
